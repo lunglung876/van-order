9cb0f82d61fd8c36de1e4c3b7e45a414b4fa54a2
Eric Lai <eric.lai@101medialab.com>
2019-07-15 11:42:37 +0800

    Add place order API

---
+++ code/src/Controller/OrderController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use GuzzleHttp\Client;
+use FOS\RestBundle\Controller\Annotations\View;
+
+class OrderController extends AbstractFOSRestController
+{
+    /**
+     * @Route("/orders", name="order_create", methods={"POST"})
+     * @View()
+     */
+    public function create(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    {
+        $origin = $request->get('origin');
+        $destination = $request->get('destination');
+        $order = $this->createOrder($origin, $destination, $entityManager, $validator);
+
+        return [
+            'id' => $order->getId(),
+            'distance' => $order->getDistance(),
+            'status' => $order->getStatus()
+        ];
+    }
+
+    private function createOrder($origin, $destination, EntityManagerInterface $entityManager, ValidatorInterface $validator) : Order
+    {
+        if (empty($origin[0]) || empty($origin[1]) || count($origin) > 2 || !is_string($origin[0]) || !is_string($origin[1])) {
+            throw new BadRequestHttpException('Incorrect origin.');
+        }
+
+        if (empty($destination[0]) || empty($destination[1]) || count($destination) > 2 || !is_string($destination[0]) ||
+            !is_string($destination[1])
+        ) {
+            throw new BadRequestHttpException('Incorrect destination.');
+        }
+
+        $order = new Order();
+        $order->setStatus(Order::STATUS_UNASSIGNED);
+        $order->setCreatedAt(new \DateTime());
+        $order->setOriginLatitude($origin[0]);
+        $order->setOriginLongitude($origin[1]);
+        $order->setDestinationLatitude($destination[0]);
+        $order->setDestinationLongitude($destination[1]);
+
+        // Validate origin and destination
+        $violations = $validator->validate($order);
+
+        if ($violations->count() > 0) {
+            $violation = $violations[0];
+
+            throw new BadRequestHttpException(sprintf('%s: %s', $violation->getInvalidValue(), $violation->getMessage()));
+        }
+
+        $order->setDistance($this->getDistance($origin, $destination));
+        $entityManager->persist($order);
+        $entityManager->flush();
+
+        return $order;
+    }
+
+    private function getDistance($origin, $destination) : int
+    {
+        $client = new Client();
+        $response = $client->get('https://maps.googleapis.com/maps/api/distancematrix/json', [
+            'query' => [
+                'origins' => implode($origin, ','),
+                'destinations' => implode($destination, ','),
+                'key' => $this->getParameter('google_maps_api_key')
+            ]
+        ]);
+
+        $response = json_decode($response->getBody(), true);
+
+        if (!isset($response['rows'][0]['elements'][0]['status']) || $response['rows'][0]['elements'][0]['status'] !== 'OK') {
+            throw new BadRequestHttpException('Cannot calculate distance');
+        }
+
+        return $response['rows'][0]['elements'][0]['distance']['value'];
+    }
+}
